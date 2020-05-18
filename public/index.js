970272e35331fa5da59c4a50804fdb5ac26bac67
shivasairam.mummoju <shivasairam.mummoju@servicenow.com>
2020-05-19 01:13:35 +0530

    url

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://innovationday-app.herokuapp.com/'); 
+var socket = io.connect(`http://127.0.0.1:${process.env.PORT || 5000}`); 
 
 var answersFrom = {}, offer;
 var peerConnection = window.RTCPeerConnection ||
